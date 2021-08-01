ca8d92c0d628cc9567521feaa09c6ec786c23841
Banjo Victor <victorolaolubanjo@yahoo.com>
2021-08-01 21:49:05 +0100

    Update Util.dart

--- lib/util/Util.dart
+++ lib/util/Util.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
 class Util {
-  static String apiKey = 'e2a58929ac804733a31200802210707';
+  static String apiKey = '';
 
   static String getTime(DateTime date) {
     return DateFormat('EEEE, MMM d, y').format(date);
